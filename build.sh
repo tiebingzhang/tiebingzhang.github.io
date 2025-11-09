2aa7a87d08dd9a08b59d670dbbd6eab30fa91fa8
Tiebing Zhang <awszhang@amazon.com>
2025-11-09 12:15:12 -0800

    update

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-bundle exec jekyll build
+#!/bin/bash
+bundle exec jekyll build 2>&1 | grep -v "Deprecation Warning \[import\]" | grep -v "More info and automated migrator" | grep -v "DEPRECATION WARNING"  
